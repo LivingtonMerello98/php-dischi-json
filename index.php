c74c8a961172417fafa57a4ee77590351b8cce29
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-20 17:36:00 +0200

    [upd] axios cdn and albums array

--- index.php
+++ index.php
@@ -10,10 +10,19 @@
 
 <body>
     <main id="app">
-        <h1>{{ message }}</h1>
+        <h1>{{ title }}</h1>
+
+        <ul>
+            <li v-for=" album in albums">
+                <h3>{{ album.album }}</h3>
+            </li>
+        </ul>
+
     </main>
     <!-- bootsrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <!--vue cdn-->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="main.js"></script>
